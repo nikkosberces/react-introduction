915b521b91bcd6d407d559128f0d1b8e2fbdcff9
nikkosberces <nikkosberces@gmail.com>
2023-04-25 21:11:56 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import FullName from "./FullName";
+
+function App() {
+  return (
+    <div>
+      <FullName name="Nikko Berces" />
+      <FullName name="Carlo Dacuyan" />
+    </div>
+  );
+}
+
+export default App;
